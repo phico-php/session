3d519867f41087ee91b5159d9ff7f0fcb4c7af29
indgy <65541311+indgy@users.noreply.github.com>
2024-08-09 09:01:58 +0100

    Fix fetchOrCreate signature to support null id

--- src/Store/StoreInterface.php
+++ src/Store/StoreInterface.php
@@ -13,7 +13,7 @@ interface StoreInterface
     public function delete(string $id): bool;
     public function exists(string $id): bool;
     public function fetch(string $id): Session;
-    public function fetchOrCreate(string $id): Session;
+    public function fetchOrCreate(null|string $id = null): Session;
     public function regenerate(Session $session): Session;
     public function store(Session $session): bool;
 }
